d5203c4f4e78e8e7198639b87bb6f0c5522ce39a
Ha-ily123 <hailyhaily519@gmail.com>
2026-03-26 16:02:41 +0530

    code finsh

--- ss_main/smart_scheduler/models.py
+++ ss_main/smart_scheduler/models.py
@@ -7,10 +7,7 @@ class ManualSchedule(models.Model):
     offTime=models.TimeField()
     status = models.BooleanField(default=True)
 
-    # class Meta:
-    #     verbose_name = "Manual Schedule"
-    #     verbose_name_plural = "Manual Schedules"
-
+    
 
 class Motion(models.Model):
     """This controls motion control mode"""
@@ -18,9 +15,7 @@ class Motion(models.Model):
     offDelay=models.IntegerField()
     status=models.BooleanField(default=True)
 
-    # class Meta:
-    #     verbose_name = "Motion Schedule"
-    #     verbose_name_plural = "Motion Schedules"
+    
 
 
 class RelayControls(models.Model):
@@ -28,19 +23,12 @@ class RelayControls(models.Model):
     light=models.BooleanField(default=True)
     fan=models.BooleanField(default=True)
 
-    # class Meta:
-    #     verbose_name = "Lights ON OFF control"
-    #     verbose_name_plural = "Lights ON OFF controls"
-
+    
 
 class SensorData(models.Model):
     """Stores data from the pico"""
     sensor_value=models.IntegerField()
     time_stamp=models.DateTimeField( auto_now_add=True)
-
-    # class Meta:
-    #     verbose_name = "Motion mode control"
-    #     verbose_name_plural = "Motion mode controls"
 
 
 
--- ss_main/ss_main/urls.py
+++ ss_main/ss_main/urls.py
@@ -1,7 +1,10 @@
 from django.contrib import admin
 from django.urls import path,include
+from smart_scheduler.views import loginForm
+
 
 urlpatterns = [
     path('admin/', admin.site.urls),
-    path('api/',include('smart_scheduler.urls')),
+    path('api/', include('smart_scheduler.urls')),
+    path("", loginForm),
 ]
